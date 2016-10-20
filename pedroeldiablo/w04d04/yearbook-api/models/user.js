6e4e1844fa27959f935761dc844cbd894a1452db
Peter Williams <pedroeldiablo@yahoo.com>
2016-10-20 19:29:14 +0100

    name required working

--- pedroeldiablo/w04d04/yearbook-api/models/user.js
+++ pedroeldiablo/w04d04/yearbook-api/models/user.js
@@ -1,8 +1,8 @@
 const mongoose =require('mongoose');
 
 const userSchema = mongoose.Schema({
-name: {type: String, trim:true},
-github: {type: String, trim:true},
+name: {type: String, trim:true, required: true},
+github: {type: String, trim:true, unique:true},
 image: {type: String, trim:true},
 bio: {type: String, trim:true},
 portfolio: {type: String, trim:true},
