55bf10870bcb88fb6409a853c93d457968442970
Peter Williams <pedroeldiablo@yahoo.com>
2016-10-24 00:23:10 +0100

    able to get ajax to display using the console, but not print to page. Unsure of the error.

--- pedroeldiablo/w04d05/public/js/dist/client.js
+++ pedroeldiablo/w04d05/public/js/dist/client.js
@@ -1,3 +1,37 @@
-"use strict";
+'use strict';
 
-console.log("JS loaded!");
+$(function () {
+  loadLines();
+
+  $('form').on('submit', getLines);
+});
+
+var loadLines = function loadLines() {
+  $.ajax({
+    method: 'GET',
+    url: "http://localhost:8000/lines"
+  }).done(function (res) {
+    console.log(res.data);
+    $.each(res.data, function (index, line) {
+      addLine(line);
+    });
+  });
+};
+
+var addLine = function addLine(line) {
+  $('#lines').prepend('<li>"$line.name"</li>');
+};
+
+var getLines = function getLines(e) {
+  e.preventDefault();
+  var linesearch = $('.linesearch').val();
+  $.ajax({
+    method: 'GET',
+    url: "http://localhost:8000" + linesearch
+  }).done(function (res) {
+    console.log(res.data);
+    $.each(res.data, function (index, line) {
+      addLine(line);
+    });
+  });
+};
--- pedroeldiablo/w04d05/public/js/src/client.js
+++ pedroeldiablo/w04d05/public/js/src/client.js
@@ -1,1 +1,45 @@
-console.log("JS loaded!");
+// $(function(){
+
+  // loadLines();
+
+  // $('form').on('submit', getLines);
+
+// });
+
+  const loadLines = () => {
+    $.ajax({
+      method:'GET',
+      url: "http://localhost:8000/lines"
+    })
+    .done((res) => {
+      console.log(res);
+      $.each(res, (index, line) => {
+        addLine(line);
+      });
+    });
+  };
+
+  loadLines();
+
+
+  const addLine = (line) => {
+    $('#lines').prepend(`<li>${line.name}</li>`);
+  };
+
+
+
+  // const getLines = (e) => {
+  //   e.preventDefault();
+  //   let linesearch = $('.linesearch').val();
+  //   $.ajax({
+  //     method:'GET',
+  //     url: "http://localhost:8000/lines" + linesearch
+  //   })
+  //   .done((res) => {
+  //     console.log(res.data);
+  //     $.each(res.data, (index, line) => {
+  //       addLine(line);
+  //
+  //     });
+  //   });
+  // };
