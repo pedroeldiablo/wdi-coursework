10caf5862184397deeb538c96670d3be6e46b841
Peter Williams <pedroeldiablo@yahoo.com>
2016-10-24 18:47:51 +0100

    markers showing

--- pedroeldiablo/w05d01 Homework/restaurants/public/js/app.js
+++ pedroeldiablo/w05d01 Homework/restaurants/public/js/app.js
@@ -1,33 +1,13 @@
 "use strict";
 
-$(function () {
-  console.log("JQuery loaded");
-});
+// $(() =>{
+//   console.log("JQuery loaded");
+// });
 var googleMap = googleMap || {};
 
-googleMap.loopThroughRestaurants = function (data) {
-  $.each(data.restaurant, function (index, restaurant) {
-    googleMap.createMarkerForRestaurant(restaurant);
-  });
-};
-
-googleMap.getRestaurants = function () {
-  $.get("http://localhost:3000/api/restaurants").done(this.loopThroughRestaurants);
-};
-
-googleMap.addInfoWindowForRestaurant = function (restaurant, marker) {
-  var _this = this;
-
-  google.maps.event.addListener(marker, "click", function () {
-    if (_this.infowindow) {
-      _this.infowindow.close();
-    }
-    _this.infowindow = new google.maps.InfoWindow({
-      content: "\n      <p>" + restaurant.name + "</p>\n      <p>" + restaurant.location + "</p>"
-    });
-    _this.infowindow.open(_this.map, marker);
-  });
-};
+// googleMap.getRestaurants = function () {
+//   $.get("http://localhost:3000/api/restaurants").done((data) => console.log(data.restaurants));
+// };
 
 googleMap.mapSetup = function () {
   var canvas = document.getElementById("map-canvas");
@@ -41,13 +21,42 @@ googleMap.mapSetup = function () {
   this.getRestaurants();
 };
 
+$(googleMap.mapSetup.bind(googleMap));
+
+googleMap.loopThroughRestaurants = function (data) {
+  $.each(data.restaurants, function (index, restaurant) {
+    console.log(restaurant);
+    googleMap.createMarkerForRestaurant(restaurant);
+  });
+};
+
+googleMap.getRestaurants = function () {
+  $.get("http://localhost:3000/api/restaurants").done(this.loopThroughRestaurants);
+};
+
+// googleMap.addInfoWindowForRestaurant = function (restaurant, marker){
+//   google.maps.event.addListener(marker, "click", () => {
+//     if (this.infowindow){
+//       this.infowindow.close();
+//     }
+//     this.infowindow = new google.maps.InfoWindow({
+//       content: `
+//       <p>${restaurant.name}</p>
+//       <p>${restaurant.location}</p>`
+//     });
+//     this.infowindow.open(this.map, marker);
+//   });
+// };
+
+//
+//
 googleMap.createMarkerForRestaurant = function (restaurant) {
   var latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
   var marker = new google.maps.Marker({
     position: latLng,
     map: googleMap.map
   });
-  // googleMap.addInfoWindowForCamera(camera, marker);
+  // googleMap.addInfoWindowForRestaurant(restaurant, marker);
 };
 
-$(googleMap.mapSetup.bind(googleMap));
+// $(googleMap.mapSetup.bind(googleMap));
--- pedroeldiablo/w05d01 Homework/restaurants/src/js/app.js
+++ pedroeldiablo/w05d01 Homework/restaurants/src/js/app.js
@@ -1,54 +1,62 @@
-$(() =>{
-  console.log("JQuery loaded");
-});
-var googleMap = googleMap || {};
+// $(() =>{
+//   console.log("JQuery loaded");
+// });
+  var googleMap = googleMap || {};
+
+  // googleMap.getRestaurants = function () {
+  //   $.get("http://localhost:3000/api/restaurants").done((data) => console.log(data.restaurants));
+  // };
+
+  googleMap.mapSetup = function () {
+    let canvas = document.getElementById("map-canvas");
+
+    let mapOptions = {
+      zoom: 16,
+      center: new google.maps.LatLng(51.5, -0.08),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    this.map = new google.maps.Map(canvas, mapOptions);
+    this.getRestaurants();
+  };
+
+  $(googleMap.mapSetup.bind(googleMap));
 
 googleMap.loopThroughRestaurants = (data) => {
-  $.each(data.restaurant, (index, restaurant) => {
+  $.each(data.restaurants, (index, restaurant) => {
+    console.log(restaurant);
     googleMap.createMarkerForRestaurant(restaurant);
   });
 };
 
 googleMap.getRestaurants = function (){
-  $.get("http://localhost:3000/api/restaurants")
-    .done(this.loopThroughRestaurants);
+  $.get("http://localhost:3000/api/restaurants").done(this.loopThroughRestaurants);
 };
 
-googleMap.addInfoWindowForRestaurant = function (restaurant, marker){
-  google.maps.event.addListener(marker, "click", () => {
-    if (this.infowindow){
-      this.infowindow.close();
-    }
-    this.infowindow = new google.maps.InfoWindow({
-      content: `
-      <p>${restaurant.name}</p>
-      <p>${restaurant.location}</p>`
-    });
-    this.infowindow.open(this.map, marker);
-  });
-};
+// googleMap.addInfoWindowForRestaurant = function (restaurant, marker){
+//   google.maps.event.addListener(marker, "click", () => {
+//     if (this.infowindow){
+//       this.infowindow.close();
+//     }
+//     this.infowindow = new google.maps.InfoWindow({
+//       content: `
+//       <p>${restaurant.name}</p>
+//       <p>${restaurant.location}</p>`
+//     });
+//     this.infowindow.open(this.map, marker);
+//   });
+// };
 
-googleMap.mapSetup = function () {
-  let canvas = document.getElementById("map-canvas");
-
-  let mapOptions = {
-    zoom: 16,
-    center: new google.maps.LatLng(51.5, -0.08),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  this.map = new google.maps.Map(canvas, mapOptions);
-  this.getRestaurants();
-};
-
+//
+//
 googleMap.createMarkerForRestaurant = (restaurant) => {
   let latLng = new google.maps.LatLng(restaurant.lat, restaurant.lng);
   let marker = new google.maps.Marker({
     position: latLng,
     map: googleMap.map,
-    // icon:'images/marker.png'
+    // icon
   });
-  // googleMap.addInfoWindowForCamera(camera, marker);
+  // googleMap.addInfoWindowForRestaurant(restaurant, marker);
 };
 
 
-$(googleMap.mapSetup.bind(googleMap));
+// $(googleMap.mapSetup.bind(googleMap));
