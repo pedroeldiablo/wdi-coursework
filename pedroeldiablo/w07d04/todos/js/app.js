06f1ada57d144a4c530149ccb7255e24b8bcf749
Peter Williams <pedroeldiablo@yahoo.com>
2016-11-10 17:56:48 +0000

    router working

---
+++ pedroeldiablo/w07d04/todos/js/app.js
@@ -0,0 +1,19 @@
+angular
+	.module('todoApp', ['ui.router'])
+  .config(MainRouter);
+
+MainRouter.$inject = ['$stateProvider', '$urlRouterProvider'];
+function MainRouter($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'home.html'
+    })
+    .state('archive', {
+      url: '/archive',
+      templateUrl: 'archive.html'
+    });
+
+  $urlRouterProvider.otherwise('/');
+    // ROUTE
+}
