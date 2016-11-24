28ea34738e46ed492581520edf2fcfa2d3c70afc
Peter Williams <pedroeldiablo@yahoo.com>
2016-11-24 18:22:05 +0000

    mortgage calculator working

--- pedroeldiablo/w09d04/calculator/main.rb
+++ pedroeldiablo/w09d04/calculator/main.rb
@@ -1,36 +1,64 @@
 calculating = true
 while calculating do
-  puts "What sum would you like to do? Type +, -, *, /, power, root"
+  puts "What sum would you like to do? Type +, -, *, /, power, root, weight-on-moon, mortgage"
   answer = gets.chomp
-  puts "What is your first number?"
+  if answer ="mortgage"
+    puts "How much do you want to borrow?"
+    borrow = gets.to_f
+    puts "How long do you want to pay back over in years?"
+    duration = gets.to_f
+    puts "What is the interest rate?"
+    rate = gets.to_f
+    monthrate = (rate/12)/100
+    puts monthrate
+    payments = duration * 12
+    puts payments
+    part1 = (1 + monthrate) ** payments
+    puts part1
+    top = monthrate * part1
+    puts top
+    bottom = part1 - 1
+    puts bottom
+    fraction = top / bottom
+    puts fraction
+    monthly = borrow * fraction
+    puts "£#{monthly}"
 
-  num1 = gets.to_f
-  puts "What is your second number?"
-  num2 = gets.to_f
+  elsif answer == "weight-on-moon"
+    puts "How much do you weigh?"
+    weight = gets.to_f
+    puts weight*0.2
+  elsif answer =="root"
+      puts "Which number do you want to know the root of?"
+      rootof = gets.to_f
+      puts Math.sqrt(rootof)
+  else
+    puts "What is your first number?"
 
-  case answer
-  when "+"
-    puts "addition"
-    puts num1 + num2
+    num1 = gets.to_f
+    puts "What is your second number?"
+    num2 = gets.to_f
 
-  when "-"
-    puts "subtraction"
-    puts num1 - num2
-  when "*"
-    puts "multiplication"
-    puts num1 * num2
-  when "/"
-    puts "division"
-    puts num1 / num2
-  when "power"
-    puts "#{num1} to the power #{num2}"
-    puts num1 ** num2
-  when "root"
-    puts "root of #{num1}"
-    puts Math.sqrt(num1)
+    case answer
+    when "+"
+      puts "addition"
+      puts num1 + num2
+
+    when "-"
+      puts "subtraction"
+      puts num1 - num2
+    when "*"
+      puts "multiplication"
+      puts num1 * num2
+    when "/"
+      puts "division"
+      puts num1 / num2
+    when "power"
+      puts "#{num1} to the power #{num2}"
+      puts num1 ** num2
+    end
   end
   puts "Do you want to calculate again? (y/n)"
   calculating = gets.chomp == 'y'
 end
-
-puts "OK, thanks for playing!"
+puts "OK, thanks!"
